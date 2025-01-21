1b8cffa69b509e745a4e4c36fc7c287a66e53ed7
julianojosoa13 <josoajuliano@gmail.com>
2025-01-22 00:35:44 +0300

    Adds ShadcnUI

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,18 @@ import Image from "next/image";
 import Lottie from "react-lottie-player";
 
 import lottieJson from "@/assets/lottie/shop.json";
+import { Button } from "@/components/ui/button";
+import {
+  ArrowBigRightDash,
+  ArrowRightToLine,
+  ArrowUpRight,
+} from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-8 row-start-2 items-center justify-center">
         <Image
           src="/trans_bg.png"
           alt="Logo Ahofa App"
@@ -15,12 +22,25 @@ export default function Home() {
           height={220}
           className="self-center mx-auto my-6"
         />
+
         <Lottie
           loop
           animationData={lottieJson}
           play
-          style={{ width: 180, height: 180 }}
+          style={{
+            width: 180,
+            height: 180,
+
+            marginBottom: 35,
+            marginLeft: "auto",
+            marginRight: "auto",
+          }}
         />
+        <Button variant={"outline"} asChild>
+          <Link href={"/tos"}>
+            Conditions Générales d'utilisation <ArrowUpRight size={22} />
+          </Link>
+        </Button>
       </main>
     </div>
   );

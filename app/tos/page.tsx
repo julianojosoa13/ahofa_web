64fe71cfb1867fb483c3f266b009cced3efe3c75
julianojosoa13 <josoajuliano@gmail.com>
2025-01-21 23:27:30 +0300

    Terms of Service page

---
+++ app/tos/page.tsx
@@ -0,0 +1,186 @@
+import React from "react";
+
+const Tos = () => {
+  return (
+    <div>
+      <article>
+        <h2>1. Ownership of Content</h2>
+        <p>
+          1.1 Users retain full ownership of all content and data they share on
+          the app.
+        </p>
+        <p>
+          1.2 Users have the right to remove their content at any time or report
+          stolen personal data or information.
+        </p>
+        <p>
+          1.3 The app serves as a platform only and does not assume ownership of
+          user content.
+        </p>
+      </article>
+
+      <article>
+        <h2>2. Regulation of Content</h2>
+        <p>
+          2.1 The app strives to regulate content to ensure a safe and
+          trustworthy environment.
+        </p>
+        <p>
+          2.2 Users are solely responsible and accountable for the content they
+          share on the app.
+        </p>
+      </article>
+
+      <article>
+        <h2>3. Use of Personal Data</h2>
+        <p>
+          3.1 Users agree to the use of some of their data for advertising
+          purposes.
+        </p>
+        <p>
+          3.2 The app does not track personal data but may use behavioral data
+          to provide personalized item suggestions based on user habits.
+        </p>
+        <p>
+          3.3 The app does not share personal data with third parties unless
+          required by law.
+        </p>
+      </article>
+
+      <article>
+        <h2>4. User Accountability</h2>
+        <p>
+          4.1 Users are fully responsible for ensuring that any content they
+          share on the app is lawful and does not infringe on the rights of
+          others.
+        </p>
+        <p>
+          4.2 The app assumes no responsibility for the accuracy or legality of
+          user-shared content.
+        </p>
+      </article>
+
+      <h1>Conditions d'Utilisation</h1>
+
+      <article>
+        <h2>1. Propriété du Contenu</h2>
+        <p>
+          1.1 Les utilisateurs conservent la pleine propriété de tout le contenu
+          et les données qu'ils partagent sur l'application.
+        </p>
+        <p>
+          1.2 Les utilisateurs ont le droit de supprimer leur contenu à tout
+          moment ou de signaler un vol de données personnelles.
+        </p>
+        <p>
+          1.3 L'application agit uniquement comme une plateforme et ne
+          revendique pas la propriété du contenu des utilisateurs.
+        </p>
+      </article>
+
+      <article>
+        <h2>2. Régulation du Contenu</h2>
+        <p>
+          2.1 L'application s'efforce de réguler le contenu pour garantir un
+          environnement sûr et fiable.
+        </p>
+        <p>
+          2.2 Les utilisateurs sont seuls responsables et imputables du contenu
+          qu'ils partagent sur l'application.
+        </p>
+      </article>
+
+      <article>
+        <h2>3. Utilisation des Données Personnelles</h2>
+        <p>
+          3.1 Les utilisateurs acceptent que certaines de leurs données soient
+          utilisées à des fins publicitaires.
+        </p>
+        <p>
+          3.2 L'application ne suit pas les données personnelles, mais peut
+          utiliser des données comportementales pour proposer des suggestions
+          personnalisées basées sur les habitudes des utilisateurs.
+        </p>
+        <p>
+          3.3 L'application ne partage pas les données personnelles avec des
+          tiers sauf en cas d'obligation légale.
+        </p>
+      </article>
+
+      <article>
+        <h2>4. Responsabilité des Utilisateurs</h2>
+        <p>
+          4.1 Les utilisateurs sont entièrement responsables de s'assurer que
+          tout contenu qu'ils partagent sur l'application est licite et ne viole
+          pas les droits des autres.
+        </p>
+        <p>
+          4.2 L'application décline toute responsabilité quant à l'exactitude ou
+          la légalité du contenu partagé par les utilisateurs.
+        </p>
+      </article>
+
+      <h1>Fepetra Fampiasana</h1>
+
+      <article>
+        <h2>1. Fananana ny Votoaty</h2>
+        <p>
+          1.1 Ny mpampiasa dia mitazona tanteraka ny fananan’izy ireo rehetra sy
+          ny angona zarainy ao amin’ny fampiharana.
+        </p>
+        <p>
+          1.2 Manan-jo hanaisotra ny votoatiny amin’ny fotoana rehetra na
+          mitatitra angon-drakitra very na nangalarina ny mpampiasa.
+        </p>
+        <p>
+          1.3 Ny fampiharana dia sehatra fotsiny ary tsy manao an’izay votoaty
+          ampiasain’ny mpampiasa ho fananany.
+        </p>
+      </article>
+
+      <article>
+        <h2>2. Fanaraha-maso ny Votoaty</h2>
+        <p>
+          2.1 Ny fampiharana dia miezaka mifehy ny votoaty mba hiantohana
+          tontolo azo antoka sy atokisana.
+        </p>
+        <p>
+          2.2 Ny mpampiasa dia tompon’andraikitra feno sy azo itokisana amin’ny
+          votoaty zarainy ao amin’ny fampiharana.
+        </p>
+      </article>
+
+      <article>
+        <h2>3. Fampiasana ny Angon-drakitra Manokana</h2>
+        <p>
+          3.1 Ny mpampiasa dia manaiky ny hampiasaina ny angon-drakitra sasany
+          ho an’ny dokambarotra.
+        </p>
+        <p>
+          3.2 Ny fampiharana dia tsy manara-maso angon-drakitra manokana fa mety
+          hampiasa angon-drakitra mikasika ny fitondran’tena mba hanolorana
+          soso-kevitra manokana mifanaraka amin’ny fahazarana ampiasain’ny
+          mpampiasa.
+        </p>
+        <p>
+          3.3 Ny fampiharana dia tsy mizara angon-drakitra manokana amin’ny
+          antoko fahatelo raha tsy hoe noho ny antony ara-dalàna.
+        </p>
+      </article>
+
+      <article>
+        <h2>4. Andraikitry ny Mpampiasa</h2>
+        <p>
+          4.1 Ny mpampiasa dia tompon’andraikitra feno amin’ny fanamarinana fa
+          ara-dalàna ny votoaty zarainy ary tsy mandika ny zon’ny hafa.
+        </p>
+        <p>
+          4.2 Ny fampiharana dia tsy tompon’andraikitra amin’ny fahamarinan’ny
+          na ny ara-dalànan’ny votoaty zarain’ny mpampiasa.
+        </p>
+      </article>
+    </div>
+  );
+};
+
+export default Tos;
